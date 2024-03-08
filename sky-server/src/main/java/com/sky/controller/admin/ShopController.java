a056a3e347e613bdfcc2dd3ca505affb57756eb2
SE Daming <930673083@qq.com>
2024-03-08 14:54:10 +0800

    基于redis实现营业状态管理

--- sky-server/src/main/java/com/sky/controller/admin/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -1,19 +1,40 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin/shop")
 @Api(tags = "管理端店铺管理")
+@Slf4j
 public class ShopController {
+    public static final String key="SHOP_STATUS";
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @GetMapping("status")
     @ApiOperation(value = "获取营业状态")
     public Result<Integer> getStatus(){
-        return null;//TODO 用redis查询和更改
+        Object status = redisTemplate.opsForValue().get(key);
+        if(status==null){
+            redisTemplate.opsForValue().set(key,1);//若未设置状态默认为1
+        }
+        return Result.success((Integer) status);
+    }
+
+    @PutMapping("{status}")
+    @ApiOperation(value = "设置店铺状态")
+    public Result setStatus(@PathVariable int status){
+        Long id = BaseContext.getCurrentId();
+        redisTemplate.opsForValue().set(key,status);
+        return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -3,6 +3,8 @@ package com.sky.controller.user;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user/shop")
 @Api(tags = "店铺管理")
 public class ShopController {
+    public static final String key="SHOP_STATUS";
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+
     @GetMapping("status")
     @ApiOperation(value = "获取店铺状态")
     public Result<Integer>getStatus(){
-        return Result.success(1);
+        Object status = redisTemplate.opsForValue().get(key);
+        return Result.success((Integer) status);
+
     }
 }
